0c9b25388f031efe3798b046edf4b19e35ccce97
navanga <navanga@gmail.com>
2018-03-28 22:47:37 +1100

    First commit

---
+++ src/main/java/com/navanga/altar/altarbankservice/AltarBankServiceApplication.java
@@ -0,0 +1,12 @@
+package com.navanga.altar.altarbankservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltarBankServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltarBankServiceApplication.class, args);
+	}
+}
